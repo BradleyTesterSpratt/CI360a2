d35c1017b8bc4187076e9f7d7b069dc8a5c720df
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2020-04-24 14:51:47 +0100

    unirest attempt

--- app/src/main/java/uk/ac/brighton/bst19/ci360a2/OverviewActivity.java
+++ app/src/main/java/uk/ac/brighton/bst19/ci360a2/OverviewActivity.java
@@ -2,15 +2,21 @@ package uk.ac.brighton.bst19.ci360a2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
 
 public class OverviewActivity extends AppCompatActivity {
 
   private TextView titleText, scoreText;
+
+  public OverviewActivity() {
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,21 @@ public class OverviewActivity extends AppCompatActivity {
     setContentView(R.layout.activity_overview);
     titleText = findViewById(R.id.titleText);
     scoreText = findViewById(R.id.scoreText);
+    HttpResponse<String> response = null;
+    try {
+      response = restCall("title");
+    } catch (UnirestException e) {
+      e.printStackTrace();
+    }
 
     titleText.setText(title);
+    Log.println(Log.ASSERT, "response", response.toString());
   }
 
+  public HttpResponse<String> restCall(String searchText) throws UnirestException {
+    return Unirest.post("https://api-v3.igdb.com/games/")
+      .header("user-key", "49681339a8319428dd737e99fbf9681e")
+      .body("search " + searchText + "; fields name, storyline; where version_parent = null;")
+      .asString();
+  }
 }
